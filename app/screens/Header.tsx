09c331d800ddef1e3d27e7e3b069a8379167ced6
ciannu <pablo090203@gmail.com>
2024-04-16 19:53:19 +0200

    Header Update

--- app/screens/Header.tsx
+++ app/screens/Header.tsx
@@ -1,8 +1,27 @@
-import { View } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import React from "react";
 
 const Header = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Remedber</Text>
+    </View>
+  );
 };
 
 export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "#008080",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
