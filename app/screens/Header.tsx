87f3938b55e4d0027528a536bb9addf484830bab
ciannu <pablo090203@gmail.com>
2024-04-17 09:30:07 +0200

    Create 'Create Profile' Screen

--- app/screens/Header.tsx
+++ app/screens/Header.tsx
@@ -1,10 +1,43 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 const Header = () => {
+
+  const handleMenuPress = () => {
+
+  };
+
+
+  const handleProfilePress = () => {
+  
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Remedber</Text>
+      {/* Icono de menú */}
+      <TouchableOpacity onPress={handleMenuPress} style={styles.icon}>
+        <Image
+          source={require("../assets/menu_icon.png")} // Ruta de la imagen del icono de menú
+          style={styles.iconImage}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+      
+      {/* Logotipo */}
+      <Image
+        source={require("../../assets/logo.png")} // Ruta de la imagen del logotipo
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      
+      {/* Foto de perfil */}
+      <TouchableOpacity onPress={handleProfilePress} style={styles.profile}>
+        <Image
+          source={require("../assets/profile_image.png")} // Ruta de la imagen de perfil
+          style={styles.profileImage}
+          resizeMode="cover"
+        />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,15 +46,34 @@ export default Header;
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
     width: "100%",
     height: 80,
     backgroundColor: "#008080",
-    justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
+  logo: {
+    width: 120,
+    height: 40,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+  },
+  profile: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  profileImage: {
+    width: "100%",
+    height: "100%",
   },
 });
